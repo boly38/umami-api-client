edcea73a39aa8980cecab7f3e58f38037556ac99
Brice Vandeputte <brice.vandeputte@orange.com>
2022-07-03 16:51:35 +0200

    Fix #3 add c8 config .c8rc

--- tests/1_negative.test.js
+++ tests/1_negative.test.js
@@ -1,8 +1,11 @@
 import UmamiClient from '../src/UmamiClient.js';
 
+import { strict as assert } from 'assert';
 import chai from 'chai';
 const should = chai.should;
 const expect = chai.expect;
+
+var testClient = new UmamiClient({server:'https://fake.umami.exemple.com'})
 
 describe("Test UmamiClient negative cases", function() {
   before(function () {
@@ -11,6 +14,26 @@ describe("Test UmamiClient negative cases", function() {
 
   it("should not work without server" , async function() {
       expect(function () { new UmamiClient(); } ).to.throw("server is required. ie. set UMAMI_SERVER environment variable or option.");
+  });
+  it("should not work without authData" , async function() {
+      try {
+        await testClient.getSites({})
+      } catch(error) {
+        assert.equal(error, "expect valid auth data to query api")
+      }
+  });
+  it("should not work without siteDatas" , async function() {
+      expect(function () { testClient.selectSiteByDomain([]) } ).to.throw("No sites data provided");
+  });
+  it("should not work without valid siteDatas" , async function() {
+      expect(function () { testClient.selectSiteByDomain([{}]) } ).to.throw("Unexpected sites data provided");
+  });
+  it("should not work without valid siteDatas" , async function() {
+      try {
+        await testClient.getStatsForLast24h({token:'fake'}, [{}]);
+      } catch(error) {
+        assert.equal(error, "Unexpected site data provided")
+      }
   });
 
 });
